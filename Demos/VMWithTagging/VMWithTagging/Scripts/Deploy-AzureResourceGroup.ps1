f9ddfa179049432824bb6412a555e3e46c362d35
ivega@microsoft.com <ivega@microsoft.com>
2016-05-27 16:27:00 -0400

    Initial checkin
    Removed ARM content as it is covered somewhere else. Updated readme.
    Removed demo with the EA portal. Not available externally.

---
+++ Demos/VMWithTagging/VMWithTagging/Scripts/Deploy-AzureResourceGroup.ps1
@@ -0,0 +1,124 @@
+﻿#Requires -Version 3.0
+
+Param(
+  [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
+  [string] $ResourceGroupName = 'VMWithTagging',  
+  [switch] $UploadArtifacts,
+  [string] $StorageAccountName,
+  [string] $StorageAccountResourceGroupName, 
+  [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
+  [string] $TemplateFile = '..\Templates\LoadBalancedVirtualMachine.json',
+  [string] $TemplateParametersFile = '..\Templates\LoadBalancedVirtualMachine.param.dev.json',
+  [string] $ArtifactStagingDirectory = '..\bin\Debug\staging',
+  [string] $AzCopyPath = '..\Tools\AzCopy.exe',
+  [string] $DSCSourceFolder = '..\DSC'
+)
+
+Import-Module Azure -ErrorAction SilentlyContinue
+
+try {
+  [Microsoft.Azure.Common.Authentication.AzureSession]::ClientFactory.AddUserAgent("VSAzureTools-$UI$($host.name)".replace(" ","_"), "2.8.2")
+} catch { }
+
+Set-StrictMode -Version 3
+
+$OptionalParameters = New-Object -TypeName Hashtable
+$TemplateFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateFile)
+$TemplateParametersFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile)
+
+if ($UploadArtifacts)
+{
+    # Convert relative paths to absolute paths if needed
+    $AzCopyPath = [System.IO.Path]::Combine($PSScriptRoot, $AzCopyPath)
+    $ArtifactStagingDirectory = [System.IO.Path]::Combine($PSScriptRoot, $ArtifactStagingDirectory)
+    $DSCSourceFolder = [System.IO.Path]::Combine($PSScriptRoot, $DSCSourceFolder)
+
+    Set-Variable ArtifactsLocationName '_artifactsLocation' -Option ReadOnly
+    Set-Variable ArtifactsLocationSasTokenName '_artifactsLocationSasToken' -Option ReadOnly
+
+    $OptionalParameters.Add($ArtifactsLocationName, $null)
+    $OptionalParameters.Add($ArtifactsLocationSasTokenName, $null)
+
+    # Parse the parameter file and update the values of artifacts location and artifacts location SAS token if they are present
+    $JsonContent = Get-Content $TemplateParametersFile -Raw | ConvertFrom-Json
+    $JsonParameters = $JsonContent | Get-Member -Type NoteProperty | Where-Object {$_.Name -eq "parameters"}
+
+    if ($JsonParameters -eq $null) {
+        $JsonParameters = $JsonContent
+    }
+    else {
+        $JsonParameters = $JsonContent.parameters
+    }
+
+    $JsonParameters | Get-Member -Type NoteProperty | ForEach-Object {
+        $ParameterValue = $JsonParameters | Select-Object -ExpandProperty $_.Name
+
+        if ($_.Name -eq $ArtifactsLocationName -or $_.Name -eq $ArtifactsLocationSasTokenName) {
+            $OptionalParameters[$_.Name] = $ParameterValue.value
+        }
+    }
+
+	$StorageAccountKey = (Get-AzureRMStorageAccountKey -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName).Key1
+	$StorageAccountContext = (Get-AzureRmStorageAccount -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName).Context
+
+    # Create DSC configuration archive
+    if (Test-Path $DSCSourceFolder) {
+    Add-Type -Assembly System.IO.Compression.FileSystem
+        $ArchiveFile = Join-Path $ArtifactStagingDirectory "dsc.zip"
+        Remove-Item -Path $ArchiveFile -ErrorAction SilentlyContinue
+        [System.IO.Compression.ZipFile]::CreateFromDirectory($DSCSourceFolder, $ArchiveFile)
+    }
+
+    # Generate the value for artifacts location if it is not provided in the parameter file
+    $ArtifactsLocation = $OptionalParameters[$ArtifactsLocationName]
+    if ($ArtifactsLocation -eq $null) {
+        $ArtifactsLocation = $StorageAccountContext.BlobEndPoint + $StorageContainerName
+        $OptionalParameters[$ArtifactsLocationName] = $ArtifactsLocation
+    }
+
+    # Use AzCopy to copy files from the local storage drop path to the storage account container
+    & $AzCopyPath """$ArtifactStagingDirectory""", $ArtifactsLocation, "/DestKey:$StorageAccountKey", "/S", "/Y", "/Z:$env:LocalAppData\Microsoft\Azure\AzCopy\$ResourceGroupName"
+
+    # Generate the value for artifacts location SAS token if it is not provided in the parameter file
+    $ArtifactsLocationSasToken = $OptionalParameters[$ArtifactsLocationSasTokenName]
+    if ($ArtifactsLocationSasToken -eq $null) {
+       # Create a SAS token for the storage container - this gives temporary read-only access to the container (defaults to 1 hour).
+		$ArtifactsLocationSasToken = New-AzureRMStorageContainerSASToken -Container $StorageContainerName -Context $StorageAccountContext -Permission r -ExpiryTime (Get-Date).AddHours(4)
+		$ArtifactsLocationSasToken = ConvertTo-SecureString $ArtifactsLocationSasToken -AsPlainText -Force
+		$OptionalParameters[$ArtifactsLocationSasTokenName] = $ArtifactsLocationSasToken
+    }
+}
+
+# Create or update the resource group using the specified template file and template parameters file
+# Switch-AzureMode AzureResourceManager
+
+#New-AzureResourceGroup -Name $ResourceGroupName `
+#                       -Location $ResourceGroupLocation `
+#                       -TemplateFile $TemplateFile `
+#                       -TemplateParameterFile $TemplateParametersFile `
+#                        @OptionalParameters `
+#                        -Tag @( @{ Name="IO Code"; Value="10101-COGS-CLOUD-MANAGED" }) `
+#                        -Force -Verbose 
+
+New-AzureRMResourceGroup `
+    -Name $ResourceGroupName `
+    -Location $ResourceGroupLocation `
+	-Tag @( @{ Name="IO Code"; Value="10101-COGS-CLOUD-MANAGED" }) `
+    -Verbose -Force -ErrorAction Stop
+
+Test-AzureRmResourceGroupDeployment `
+    -ResourceGroupName $ResourceGroupName `
+    -TemplateFile $TemplateFile `
+    -TemplateParameterFile $TemplateParametersFile `
+	-Mode Incremental `
+    @OptionalParameters `
+    -ErrorAction Stop   
+
+New-AzureRMResourceGroupDeployment `
+    -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+    -ResourceGroupName $ResourceGroupName `
+    -TemplateFile $TemplateFile `
+    -TemplateParameterFile $TemplateParametersFile `
+	-Mode Incremental `
+    @OptionalParameters `
+    -Verbose -Force
---
+++ Demos/WebApp-Billing-MultiTenant/ISVDemoUsage/Models/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISVDemoUsage.Models
+{
+    public static class ExtensionMethods
+    {
+        public static string GetEncodedDate(this DateTime d, bool includeHour)
+        {
+            var datePart = d.ToString("yyyy-MM-dd");
+            string timePart = "00:00:00Z";
+            if(includeHour)
+            {
+                timePart = d.ToString("HH:00:00Z");
+            }
+            string ret = string.Concat(datePart, "+", HttpContext.Current.Server.UrlEncode(timePart));
+            return ret;
+        }
+    }
+}
